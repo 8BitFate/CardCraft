8573e01841f8eafb5c459df9a2d5343c709ea486
László Gácsi <8laci8@gmail.com>
2025-08-04 04:30:40 +0200

    Refactored code and moved cards logic to card handler.

--- Scripts/CardHandler.gd
+++ Scripts/CardHandler.gd
@@ -3,120 +3,55 @@ class_name CardHandler
 
 @onready var cards_node = $Cards
 var screen_size
-var lastevent
-var multiselect = false
-var multigrab_hit = false
-var multigrab_list = []
-var cursor_pos = Vector2()
+var default_scale = Vector2(1, 1)
 
-enum Shape {NOTHING, STACK, TILE, FAN}
+var hovered_cards = []  # TODO implement a priority que for this
 
-var current_shape : Shape = Shape.NOTHING
+var grabbed : Card = null
+var offset = Vector2()
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	for i in 10:
-		var card = Card.instantiate()
-		cards_node.add_child(card)
-		card.update_handler()
+	for card in cards_node.get_children():
+		connect_card_signals(card)
 
-func _process(_delta: float) -> void:
-	multigrab_hit = false
-	var selected = get_selected()
-	if Shape.STACK == current_shape:
-		stack(selected, cursor_mod())
-	elif Shape.TILE == current_shape:
-		tile(selected, cursor_mod())
-	elif Shape.FAN == current_shape:
-		fan(selected, cursor_mod())
+func _process(delta: float) -> void:
+	if hovered_cards and !grabbed:
+		hovered_cards[0].is_highlighted = true
+	if grabbed:
+		grabbed.global_position = lerp(grabbed.global_position, calculate_grabbed_position(), 15 * delta)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Stack Cards"):
-		cursor_pos = get_global_mouse_position()
-		current_shape = Shape.STACK
-	elif event.is_action_pressed("Tile Cards"):
-		cursor_pos = get_global_mouse_position()
-		current_shape = Shape.TILE
-	elif event.is_action_pressed("Fan Cards"):
-		cursor_pos = get_global_mouse_position()
-		current_shape = Shape.FAN
-	elif event.is_action_pressed("Invert Cards"):
-		invert(get_selected())
-	if event.is_action_released("Stack Cards") \
-	or event.is_action_released("Tile Cards") \
-	or event.is_action_released("Fan Cards"):
-		current_shape = Shape.NOTHING
+	if event.is_action_pressed("Grab"):
+		if hovered_cards:
+			grabbed = hovered_cards[0]
+			offset = grabbed.global_position - get_global_mouse_position()
+			grabbed.is_grabbed = true
+	if event.is_action_released("Grab"):
+		if grabbed:
+			grabbed.is_grabbed = false
+			grabbed = null
+			
+
+func connect_card_signals(card: Card):
+	card.connect(card.mouse_entered.get_name(), mouse_entered_card)
+	card.connect(card.mouse_exited.get_name(), mouse_exited_card)
 	
-func cursor_mod():
-	return get_global_mouse_position() - cursor_pos
+func mouse_entered_card(card: Card):
+	var ind = 0
+	while ind < hovered_cards.size() and card_order(hovered_cards[ind], card):
+		ind += 1
+	hovered_cards.insert(ind, card)
+	
+func mouse_exited_card(card: Card):
+	hovered_cards.erase(card)
+	card.is_highlighted = false;
 
-func stack(cards, mod):
-	print(mod)
-	var size = cards.size()
-	if 1 == size:
-		cards[0].offset = Vector2()
+func card_order (a : Node2D, b : Node2D): 
+	if a.z_index == b.z_index:
+		return a.get_index() > b.get_index()
 	else:
-		for ind in size:
-			var card : Card = cards[ind]
-			card.offset = mod / (size - 1) * ind
-			card.end_rotation = 0
-			card.calculate_offset(cursor_pos)
-
-func fan(cards, mod):
-	var size = cards.size()
-	for ind in size:
-		var card : Card = cards[ind]
-		card.offset = Vector2(
-			Game.CARD_WIDTH * 0.6 * (mod.x + 100) / 100 * ind, 
-			cos(0.1 * PI +  1.8 * PI * float(ind + 1)
-			 / float(size + 1)) * Game.CARD_HEIGHT * (mod.y + 50) / 1000 * size 
-		)
-		card.calculate_offset(cursor_pos)
-		var rot = PI / 3 * (-mod.x + 200) / 200
-		card.end_rotation = (rot / (size + 1) * (ind + 1)) - rot / 2
+		return a.z_index > b.z_index
 		
-
-func tile(cards, mod):
-	var dim : int = ceil(sqrt(cards.size()))
-	# var ratio = float(Game.CARD_WIDTH) / float(Game.CARD_HEIGHT)
-	for ind in cards.size():
-		var card = cards[ind]
-		card.offset = Vector2(
-			Game.CARD_WIDTH * 1.1 * (mod.x + 200) / 200 * (ind % dim), 
-			Game.CARD_HEIGHT * 1.06 * (mod.y + 200) / 200 * (ind / dim)
-		)
-		card.calculate_offset(cursor_pos)
-		card.end_rotation = 0
-
-func invert(cards: Array):
-	cards.reverse()
-	for card in cards:
-		card.top()
-	
-func multigrab():
-	multigrab_hit = true
-	for card in multigrab_list:
-		card.grab(false)
-	multigrab_list = []
-	
-func multigrab_cancel ():
-	multiselect = false
-	if multigrab_list:
-		for card in get_selected():
-			card.deselect()
-		multigrab_list = []
-
-func get_selected():
-	return cards_node.get_children().filter(func (card):
-		if card is Card:
-			return card.selected
-		else:
-			return false
-		)
-
-func first(event: InputEvent):
-	if event.is_match(lastevent):
-		return false
-	else:
-		lastevent = event
-		return true
+func calculate_grabbed_position(goal = get_global_mouse_position()):
+	return (goal + offset).clamp(Vector2(), screen_size)
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -3,6 +3,7 @@ class_name Game
 
 const CARD_WIDTH = 120
 const CARD_HEIGHT = 200
+const UP_SCALE = 1.05
 
 func _ready() -> void:
 	pass # Replace with function body.
