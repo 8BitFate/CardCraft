46349f10378d085df07ae2c554a4b8b63fe262e4
László Gácsi <8laci8@gmail.com>
2025-08-04 14:35:46 +0200

    Minor changes and cleanup

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -10,19 +10,25 @@ static var CardScene = preload("res://Objects/Card.tscn")
 signal mouse_entered
 signal mouse_exited
 
-enum CardStatus {DEFAULT, HOVERED, GRABBED}
+enum CardStatus {
+	DEFAULT, # nothing is happening to the card
+	HOVERED, # mouse is hovering over the card
+	GRABBED, # card is being dragged
+} 
 
-signal status(status: CardStatus)
+signal update_status(status: CardStatus)
 
+# Destination variables for lerping
 var global_position_goal : Vector2
 var display_position_goal : Vector2
 var display_scale_goal : Vector2 = Vector2.ONE
 
+## Instantiate a Card scene
 static func instantiate():
 	return CardScene.instantiate()
 
 func _ready() -> void:
-	connect(status.get_name(), status_handler)
+	connect(update_status.get_name(), status_handler)
 	global_position_goal = global_position
 
 func _process(delta: float) -> void:
--- Scripts/CardHandler.gd
+++ Scripts/CardHandler.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name CardHandler
 
 @onready var cards_node = $Cards
+
 var screen_size
 var default_scale = Vector2(1, 1)
 
@@ -15,9 +16,9 @@ func _ready() -> void:
 	for card in cards_node.get_children():
 		connect_card_signals(card)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if hovered_cards and !grabbed:
-		hovered_cards[0].status.emit(Card.CardStatus.HOVERED)
+		hovered_cards[0].update_status.emit(Card.CardStatus.HOVERED)
 	if grabbed:
 		grabbed.global_position_goal = calculate_grabbed_position()
 
@@ -26,13 +27,13 @@ func _unhandled_input(event: InputEvent) -> void:
 		if hovered_cards:
 			grabbed = hovered_cards[0]
 			offset = grabbed.global_position - get_global_mouse_position()
-			grabbed.status.emit(Card.CardStatus.GRABBED)
+			grabbed.update_status.emit(Card.CardStatus.GRABBED)
 	if event.is_action_released("Grab"):
 		if grabbed:
 			if hovered_cards.has(grabbed):
-				grabbed.status.emit(Card.CardStatus.HOVERED)
+				grabbed.update_status.emit(Card.CardStatus.HOVERED)
 			else:
-				grabbed.status.emit(Card.CardStatus.DEFAULT)
+				grabbed.update_status.emit(Card.CardStatus.DEFAULT)
 			grabbed = null
 			
 
@@ -49,7 +50,7 @@ func mouse_entered_card(card: Card):
 func mouse_exited_card(card: Card):
 	hovered_cards.erase(card)
 	if !grabbed:
-		card.status.emit(Card.CardStatus.DEFAULT)
+		card.update_status.emit(Card.CardStatus.DEFAULT)
 
 func card_order (a : Node2D, b : Node2D): 
 	if a.z_index == b.z_index:
